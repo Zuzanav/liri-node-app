d64832172159ed0031d1e3fe07a36467f1bbd40f
Zuzanav <zuzanavolny@gmail.com>
2019-08-26 15:52:05 -0700

    environment setup

---
+++ liri.js
@@ -0,0 +1,27 @@
+//=================================================================
+// REQUIRES ----------------------------------------------
+
+require('dotenv').config();
+
+var keys = require("./keys.js");
+var Spotify = require('node-spotify-api');
+var spotify = new Spotify(keys.spotify);
+
+
+
+//================================================================
+
+
+// SPOTIFY --------------------------------------------------------------------------------------
+ 
+spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+   
+  console.log(data.tracks.items[0]); 
+  });
+
+
+
+//-----------------------------------------------------------------------------------------------
